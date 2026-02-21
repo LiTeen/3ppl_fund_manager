0de1b3b1b85c8ac8ef05b722fefc582c9f3d9e4c
LiTeen <liteen.eh@gmail.com>
2026-02-21 22:10:46 +1300

    add design.md and completed models

---
+++ app/design.md
@@ -0,0 +1,35 @@
+1. Member Model 
+    - Member Name: [Teen, Jacky, WCH]
+    - Initial capital: [RM 53,081.42]
+    - Join Date: [3/7/2023]
+    - Stake: [0.4,0.4,0.2]
+    - Is Active: If the fund ever changes members, you can "deactivate" one without deleting their historical data.
+
+2. Borrower Model 
+    - Borrower Name
+
+3. Loan Model(The 3% Simple Interest)
+    - Borrower Link: Foreign key to the borrower model
+    - Principal: The original amount lent
+    - Annual Interest Rate: [0.03]
+    - Lending date: When the money left the fund
+    - Plan payback date: What borrower promised
+    - Actual payback date: [Allow NULL] When payback money received
+    - Status:(eg: Pending-PD, Paid_Early-PE, Overdue-OD, Closed-CL)
+
+4. Transaction Ledger
+    - Member link: Who is this for?
+    - Amount: Positive for money in, Negative for money out
+    - Category: Capital Withdrawal / Bank Interest Received / Loan Out/ Principal Returned / Loan Interest Received / Capital Injection
+    - Timestamp: Exact date of the entry
+    - Reference ID: (Optional: Link to a Loan ID if applicable, or leave empty for Bank Interest)
+    - Remarks: "6 months HLB @ 3.5%"
+
+5. Loan Repayment Model 
+    - Loan Link: Which loan is paid
+    - Amount Paid: Money received
+    - Payment Type: Interest only / Principal reduction
+    - Date: When the payment was received?
+    - Remarks: Reason for borrow
+
+   
---
+++ app/models.py
@@ -0,0 +1,69 @@
+import peewee as pw
+import datetime 
+
+db = pw.SqliteDatabase('fund.db')
+
+class BaseModel(pw.Model):
+    class Meta: 
+        database = db
+
+
+class Member(BaseModel):
+    name = pw.CharField(max_length=100, unique=True)
+    initial_capital = pw.DecimalField(max_digits=12, decimal_places=2)
+    join_date = pw.DateField()
+    stake = pw.DecimalField(max_digits=3, decimal_places=2)
+    is_active = pw.BooleanField(default=True)
+
+
+class Borrower(BaseModel):
+    name = pw.CharField(max_length=100,  unique=True)
+
+
+class Loan(BaseModel):
+    borrower = pw.ForeignKeyField(Borrower, backref='loans')
+    principal = pw.DecimalField(max_digits=12, decimal_places=2)
+    interest_rate = pw.DecimalField(default=0.03, max_digits=3,decimal_places=2)
+    lending_date = pw.DateField()
+    plan_payback_date = pw.DateField()
+    actual_payback_date = pw.DateField(null=True)
+
+    STATUS_CHOICES = (
+        ('pd','Pending'),
+        ('pe','Paid Early'),
+        ('od','Overdue'),
+        ('cl','Closed')
+    )
+    status = pw.CharField(default='pd', max_length=2, choices=STATUS_CHOICES)
+
+
+class Transaction_Ledger(BaseModel):
+    member = pw.ForeignKeyField(Member,backref='transactions')
+    amount = pw.DecimalField(max_digits=12,decimal_places=2)
+    timestamp = pw.DateTimeField(default = datetime.datetime.now)
+    loan_id = pw.ForeignKeyField(Loan,backref='loan_reference',null=True)
+    remarks = pw.CharField(max_length=300,null=True)
+    
+    CATEGORY_CHOICES = (
+        ('capital_withdraw', 'Capital Withdrawal'),
+        ('bank_int_received', 'Bank Interest Received'),
+        ('loan_out', 'Loan Out'),
+        ('principal_returned','Principal Returned'),
+        ('loan_int_received','Loan Interest Received'),
+        ('capital_in', 'Capital Injection')
+    )
+    category = pw.CharField(max_length=30,choices=CATEGORY_CHOICES)
+
+
+class Loan_Repayment(BaseModel):
+    loan = pw.ForeignKeyField(Loan, backref='repayments')
+    amount_paid = pw.DecimalField(max_digits=12,decimal_places=2)
+    date = pw.DateField()
+    remarks = pw.CharField(max_length=300,null=True)
+    PAYMENT_CHOICES = (
+        ('i','Interest only'),
+        ('p','Principal reduction')
+    )
+
+    payment_type = pw.CharField(max_length=1 , choices = PAYMENT_CHOICES,default = 'p')
+    
